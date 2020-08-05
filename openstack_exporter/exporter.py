260c4e668a87fb328ed8e6aa030dd0e6f529c3c8
Hemna <waboring@hemna.com>
2020-08-05 11:06:56 -0400

    Allow for dynamically loading collectors
    This patch adds a new config section to the sample yaml file
    that allows the user to specify which collectors to enable.
    This allows us to add new collector classes without the need
    to modify exporter.py

--- openstack_exporter/exporter.py
+++ openstack_exporter/exporter.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import click
+import importlib
 import logging
 import os
 import sys
@@ -22,12 +23,45 @@ from prometheus_client.core import REGISTRY
 from prometheus_client import start_http_server
 import yaml
 
-from openstack_exporter.collectors import cinderbackend
+LOG = logging.getLogger(__name__)
 
 
-def run_prometheus_server(port, openstack_config):
+def factory(module_class_string, super_cls: type = None, **kwargs):
+    """
+    :param module_class_string: full name of the class to create an object of
+    :param super_cls: expected super class for validity, None if bypass
+    :param kwargs: parameters to pass
+    :return:
+    """
+    module_name, class_name = module_class_string.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    assert hasattr(module, class_name), (
+        "class {} is not in {}".format(class_name, module_name))
+    # click.echo('reading class {} from module {}'.format(
+    #     class_name, module_name))
+    cls = getattr(module, class_name)
+    if super_cls is not None:
+        assert issubclass(cls, super_cls), (
+            "class {} should inherit from {}".format(
+                class_name, super_cls.__name__))
+    # click.echo('initialising {} with params {}'.format(class_name, kwargs))
+    obj = cls(**kwargs)
+    return obj
+
+
+def load_and_register_collectors(collector_config, openstack_config):
+    """Load all enabled collectors from config."""
+    for collector in collector_config:
+        cfg = collector_config[collector]
+        if cfg['enabled']:
+            LOG.info("Loading collector '{}'".format(cfg['collector']))
+            cls = factory(cfg['collector'], openstack_config=openstack_config)
+            REGISTRY.register(cls)
+
+
+def run_prometheus_server(port, collector_config, openstack_config):
     start_http_server(int(port))
-    REGISTRY.register(cinderbackend.CinderBackendCollector(openstack_config))
+    load_and_register_collectors(collector_config, openstack_config)
     while True:
         time.sleep(1)
 
@@ -59,23 +93,23 @@ def main(port, config):
     config_obj = get_config(config)
     exporter_config = config_obj['exporter']
     os_config = config_obj['openstack']
+    collector_config = config_obj['collectors']
 
-    log = logging.getLogger(__name__)
     if exporter_config['log_level']:
-        log.setLevel(logging.getLevelName(
+        LOG.setLevel(logging.getLevelName(
             exporter_config['log_level'].upper()))
     else:
-        log.setLevel(logging.getLevelName("INFO"))
+        LOG.setLevel(logging.getLevelName("INFO"))
 
     format = '[%(asctime)s] [%(levelname)s] %(message)s'
     logging.basicConfig(stream=sys.stdout, format=format)
 
-    log.info("Starting OpenStack Exporter on port={} config={}".format(
+    LOG.info("Starting OpenStack Exporter on port={} config={}".format(
         port,
         config
     ))
 
-    run_prometheus_server(port, os_config)
+    run_prometheus_server(port, collector_config, os_config)
 
 
 if __name__ == '__main__':
