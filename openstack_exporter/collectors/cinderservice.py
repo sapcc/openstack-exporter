6f439f10fe87b7f2f47d88a0f24fe017eb233f25
crenduchinta88 <77858438+crenduchinta88@users.noreply.github.com>
2024-02-07 13:42:53 -0500

    Update cinderservice.py

--- openstack_exporter/collectors/cinderservice.py
+++ openstack_exporter/collectors/cinderservice.py
@@ -22,13 +22,14 @@ from openstack_exporter import BaseCollector
 LOG = logging.getLogger('openstack_exporter.exporter')
 
 class CinderServiceCollector(BaseCollector.BaseCollector):
+    version = "1.0.0"
 
     def __init__(self, openstack_config, collector_config):
         super().__init__(openstack_config, collector_config)
         self.cinder_client = self._create_cinder_client()
 
     def _create_cinder_client(self):
-        """Create a Cinder client"""
+        """Create a Cinder client."""
         os_auth_url = self.config['auth_url']
         os_username = self.config['username']
         os_password = self.config['password']
@@ -44,16 +45,35 @@ class CinderServiceCollector(BaseCollector.BaseCollector):
                            user_domain_name=os_user_domain_name)
 
         sess = session.Session(auth=auth)
-        return cinder.Client('3.64', session=sess, region_name=self.region) # Adjust API version when required
+        return cinder.Client('3.64', session=sess, region_name=self.region)
+
+    def _create_cinder_client(self):
+        """Create a Cinder client."""
+        os_auth_url = self.config['auth_url']
+        os_username = self.config['username']
+        os_password = self.config['password']
+        os_project_name = self.config['project_name']
+        os_project_domain_name = self.config['project_domain_name']
+        os_user_domain_name = self.config['user_domain_name']
+
+        auth = v3.Password(auth_url=os_auth_url,
+                           username=os_username,
+                           password=os_password,
+                           project_name=os_project_name,
+                           project_domain_name=os_project_domain_name,
+                           user_domain_name=os_user_domain_name)
+
+        sess = session.Session(auth=auth)
+        return cinder.Client('3.64', session=sess, region_name=self.region)
 
     def describe(self):
         yield GaugeMetricFamily('cinder_service_status',
                                 'Current status of Cinder services (enabled/disabled)',
-                                labels=['service', 'host', 'zone'])
+                                labels=['service', 'host', 'zone', 'disabled_reason'])
         yield GaugeMetricFamily('cinder_service_state',
                                 'Current state of Cinder services (up/down)',
-                                labels=['service', 'host', 'zone'])
-
+                                labels=['service', 'host', 'zone', 'disabled_reason'])
+    
     def collect(self):
         LOG.info("Collecting Cinder service info.")
         try:
@@ -61,22 +81,25 @@ class CinderServiceCollector(BaseCollector.BaseCollector):
         except Exception as e:
             LOG.error(f"Error while collecting Cinder service metrics: {e}")
             return
-
+    
         g_status = GaugeMetricFamily('cinder_service_status',
                                      'Current status of Cinder services (enabled/disabled)',
-                                     labels=['service', 'host', 'zone'])
+                                     labels=['service', 'host', 'zone', 'disabled_reason'])
         g_state = GaugeMetricFamily('cinder_service_state',
                                     'Current state of Cinder services (up/down)',
-                                    labels=['service', 'host', 'zone'])
-
+                                    labels=['service', 'host', 'zone', 'disabled_reason'])
+    
         for service in services:
             LOG.debug(f"Service: {service.binary}, Host: {service.host}, "
-                      f"Zone: {service.zone}, Status: {service.status}, State: {service.state}")
-
+                      f"Zone: {service.zone}, Status: {service.status}, State: {service.state}, "
+                      f"Disabled Reason: {service.disabled_reason or 'N/A'}")
+    
             status_value = 1 if service.status == 'enabled' else 0
             state_value = 1 if service.state == 'up' else 0
-            g_status.add_metric([service.binary, service.host, service.zone], status_value)
-            g_state.add_metric([service.binary, service.host, service.zone], state_value)
-
+            disabled_reason = service.disabled_reason if service.disabled_reason else "N/A"
+    
+            g_status.add_metric([service.binary, service.host, service.zone, disabled_reason], status_value)
+            g_state.add_metric([service.binary, service.host, service.zone, disabled_reason], state_value)
+    
         yield g_status
         yield g_state
