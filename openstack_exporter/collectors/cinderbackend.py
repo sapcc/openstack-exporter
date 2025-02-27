68a2c9ed0d813bd485f8059a3979321f015cab85
Walter Boring IV <waboring@hemna.com>
2025-02-27 12:14:30 -0500

    [Cinder] report aggregated stats
    This updates the way we calculate stats the same way the
    cinder scheduler does after associating the pool with a
    volume type.  Also collects the aggregated stats for the fcd
    based pools.  Also reporting the aggregate_id when available
    on a pool.

--- openstack_exporter/collectors/cinderbackend.py
+++ openstack_exporter/collectors/cinderbackend.py
@@ -20,9 +20,11 @@ from cachetools import cached, TTLCache
 from cinderclient import client as cinder
 from openstack_exporter import BaseCollector
 
+from openstack_exporter.utils import cinder as cinder_utils
+
 LOG = logging.getLogger('openstack_exporter.exporter')
 
-DEFAULT_VALID_SHARDING_BACKENDS = ['standard_hdd', 'vmware']
+DEFAULT_VALID_SHARDING_BACKENDS = ['standard_hdd', 'vmware', 'vmware_fcd']
 DEFAULT_VALID_NO_SHARD_BACKENDS = []
 ALLOW_UNEXPECTED_BACKENDS_DEFAULT = False
 
@@ -125,156 +127,6 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
         yield GaugeMetricFamily('cinder_percent_free',
                                 'Cinder Percentage of available space is free.')
 
-    def calculate_capacity_factors(self,
-                                   total_capacity: float,
-                                   free_capacity: float,
-                                   provisioned_capacity: float,
-                                   thin_provisioning_support: bool,
-                                   max_over_subscription_ratio: float,
-                                   reserved_percentage: float,
-                                   thin: bool) -> dict:
-        """Create the various capacity factors of the a particular backend.
-
-        Based off of definition of terms
-        cinder-specs/specs/queens/provisioning-improvements.html
-
-        total_capacity - The reported total capacity in the backend.
-        free_capacity - The free space/capacity as reported by the backend.
-        reserved_capacity - The amount of space reserved from the total_capacity
-        as reported by the backend.
-        total_reserved_available_capacity - The total capacity minus reserved
-        capacity
-
-        max_over_subscription_ratio - as reported by the backend
-        total_available_capacity - The total capacity available to cinder
-        calculated
-        thick: total_reserved_available_capacity
-        OR
-        thin: total_reserved_available_capacity and max_over_subscription_ratio
-
-        provisioned_capacity - as reported by backend or volume manager
-        (allocated_capacity_gb)
-
-        calculated_free_capacity - total_available_capacity - provisioned_capacity
-        virtual_free_capacity - The calculated free capacity available to cinder
-        to allocate new storage.
-        For thin: calculated_free_capacity
-        For thick: the reported free_capacity can be less than the calculated
-        Capacity, so we use free_capacity - reserved_capacity.
-
-        free_percent - the percentage of the virtual_free and
-        total_available_capacity is left over
-        provisioned_ratio - The ratio of provisioned storage to
-        total_available_capacity
-        """
-
-        total = float(total_capacity)
-        reserved = float(reserved_percentage) / 100
-        reserved_capacity = math.floor(total * reserved)
-        total_reserved_available = total - reserved_capacity
-
-        if thin and thin_provisioning_support:
-            total_available_capacity = (
-                total_reserved_available * max_over_subscription_ratio
-            )
-            calculated_free = total_available_capacity - provisioned_capacity
-            virtual_free = calculated_free
-            provisioned_type = 'thin'
-        else:
-            # Calculate how much free space is left after taking into
-            # account the reserved space.
-            total_available_capacity = total_reserved_available
-            calculated_free = total_available_capacity - provisioned_capacity
-            virtual_free = calculated_free
-            if free_capacity < calculated_free:
-                virtual_free = free_capacity
-            provisioned_type = 'thick'
-
-        if total_available_capacity:
-            provisioned_ratio = provisioned_capacity / total_available_capacity
-            free_percent = (virtual_free / total_available_capacity) * 100
-        else:
-            provisioned_ratio = 0
-            free_percent = 0
-
-        return {
-            "total_capacity": total,
-            "free_capacity": free_capacity,
-            "reserved_capacity": reserved_capacity,
-            "total_reserved_available_capacity": int(total_reserved_available),
-            "max_over_subscription_ratio": (
-                max_over_subscription_ratio if provisioned_type == 'thin' else None
-            ),
-            "total_available_capacity": int(total_available_capacity),
-            "provisioned_capacity": provisioned_capacity,
-            "calculated_free_capacity": int(calculated_free),
-            "virtual_free_capacity": int(virtual_free),
-            "free_percent": free_percent,
-            "provisioned_ratio": provisioned_ratio,
-            "provisioned_type": provisioned_type
-        }
-
-    def _parse_pool_data(self, pool, volume_type):
-        """Construct the data from the pool information from the scheduler."""
-        caps = pool['capabilities']
-        shard_name = caps.get('vcenter-shard')
-
-        data = {"backend": caps["volume_backend_name"],
-                "pool": pool['name'].split('#')[1],
-                "shard": shard_name}
-        can_overcommit = False
-
-        # Only allow overcommit if the volume type that matches
-        # The backend is thin provisioned.
-        # A missing key of provisioning:type means thin provisioning.
-        if volume_type and volume_type['extra_specs'].get('provisioning:type') != 'thick':
-            can_overcommit = True
-
-        total_capacity_gb = caps.get('total_capacity_gb', 0)
-        allocated_capacity_gb = caps.get('allocated_capacity_gb', 0)
-        reserved_percentage = caps.get('reserved_percentage', 0)
-        max_over_subscription_ratio = float(
-            caps.get('max_over_subscription_ratio', 1)
-        )
-        free_capacity_gb = caps.get('free_capacity_gb', 0)
-
-        capacity_factors = self.calculate_capacity_factors(
-            total_capacity_gb,
-            free_capacity_gb,
-            allocated_capacity_gb,
-            caps.get('thin_provisioning_support', False),
-            max_over_subscription_ratio,
-            reserved_percentage,
-            can_overcommit,
-        )
-        available_capacity_gb = capacity_factors["total_available_capacity"]
-        virtual_free_gb = capacity_factors["virtual_free_capacity"]
-        percent_left = capacity_factors["free_percent"]
-        overcommit_ratio = capacity_factors["provisioned_ratio"]
-
-        data["can_overcommit"] = can_overcommit
-        data['total_capacity_gb'] = total_capacity_gb
-        data['max_over_subscription_ratio'] = max_over_subscription_ratio
-        data['provisioned_capacity_gb'] = caps.get('provisioned_capacity_gb', 0)
-        data['overcommit_ratio'] = overcommit_ratio
-
-        data['available_capacity_gb'] = available_capacity_gb
-        data['allocated_capacity_gb'] = allocated_capacity_gb
-        # What the backend is reporting
-        data['free_capacity_gb'] = free_capacity_gb
-        # What cinder can use
-        data['virtual_free_capacity_gb'] = virtual_free_gb
-        data['percent_left'] = percent_left
-        data['reserved_percentage'] = reserved_percentage
-
-        if can_overcommit:
-            data['provisioning_type'] = 'thin'
-        else:
-            data['provisioning_type'] = 'thick'
-
-        data['driver_version'] = caps['driver_version']
-        return data
-
     def _debug_gauge(self, gauge, name, value, shard, backend, pool):
         LOG.debug(f"({shard}/{backend}/{pool})-{name} = {value}")
         return gauge
@@ -293,6 +145,9 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
 
     def _report_stats(self, shard_name, backend, data, caps, quota_obj):
         pool_name = data['pool']
+        LOG.debug(f"Reporting stats for {shard_name}/{backend}/{pool_name}")
+        LOG.debug(f"Data {data}")
+        LOG.debug(f"Capabilities {caps}")
         yield self.add_gauge_metric_gauge(
             'cinder_per_volume_gigabytes',
             'Cinder max volume size in GiB',
@@ -309,6 +164,17 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
             'cinder_pool_state',
             'State of cinder pool up/down',
             {'pool_state': caps.get('pool_state', 'down')},
+            shard_name, backend, pool_name
+        )
+        down_reason = caps.get('pool_down_reason', 'unknown')
+        if caps.get('backend_state', 'down') == 'down':
+            down_reason = 'backend_down'
+        elif 'Datastore marked as draining' in down_reason:
+            down_reason = 'draining'
+        yield self.add_info_metric_gauge(
+            'cinder_pool_down_reason',
+            'Reason for pool state being down',
+            {'pool_down_reason': down_reason},
             shard_name, backend, pool_name
         )
 
@@ -359,6 +225,15 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
             shard_name, backend, pool_name
         )
 
+        aggregate_id = data.get("aggregate_id")
+        if aggregate_id:
+            yield self.add_info_metric_gauge(
+                'cinder_aggregate_id',
+                'Cinder aggregate id',
+                {'aggregate_id': aggregate_id},
+                shard_name, backend, pool_name
+            )
+
         if can_overcommit:
             yield self.add_gauge_metric_gauge(
                 'cinder_max_oversubscription_ratio',
@@ -396,6 +271,61 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
                 shard_name, backend, pool_name
             )
 
+    def _report_aggregated_stats(self, backend):
+        pool_name = backend['name']
+        LOG.debug(f"Reporting stats for {backend}/{pool_name}")
+        LOG.debug(f"Aggregate Data {backend}")
+
+        yield self.add_info_metric_gauge(
+            'cinder_aggregate_id',
+            'Cinder aggregate id',
+            {'aggregate_id': backend['aggregate_id']},
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_total_capacity_gib',
+            'Cinder total capacity in GiB',
+            backend['total_capacity_gb'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_free_capacity_gib',
+            'Cinder reported free capacity in GiB',
+            backend['free_capacity_gb'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_allocated_capacity_gib',
+            'Cinder allocated capacity in GiB',
+            backend['allocated_capacity_gb'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_max_oversubscription_ratio',
+            'Cinder max overcommit ratio',
+            backend['max_over_subscription_ratio'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_free_percent',
+            'Cinder Percentage of available space is free.',
+            backend['free_percent'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+        yield self.add_gauge_metric_gauge(
+            'cinder_virtual_free_capacity_gib',
+            'Cinder virtual free capacity in GiB',
+            backend['virtual_free_capacity_gb'],
+            'aggregate', 'aggregate', pool_name
+        )
+
+
     def collect(self):
         """This is the collector for cinder backends.
 
@@ -417,13 +347,15 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
         project_id = self.client.volume.get_project_id()
         quota_obj = self.cinder_client.quotas.defaults(project_id)
 
-        v_types = list(self.client.volume.types())
-        # Ignore volume types that aren't tied to a backend.
-        volume_types = (
-            {vt['extra_specs']['volume_backend_name']:
-             vt for vt in v_types if 'volume_backend_name' in vt['extra_specs']})
-
-        pools = self.client.volume.backend_pools()
+        volume_types = cinder_utils.get_volume_types(self.client)
+        # Now filter the pools through the volumes types to get the
+        # pools associated with a volume type.
+        pools = cinder_utils.filter_pools(
+            self.client,
+            cinder_utils.get_cinder_pools(self.client)
+        )
+        # build the aggregated pools list so we can report them as well.
+        aggregated_pools = cinder_utils.aggregate_pools(pools)
 
         # Keep a count of each backend in each shard for each pool.
         # If we don't see any pools in a shard/backend then it's down and
@@ -439,42 +371,44 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
             seen_backends[shard_name] = default_shard_backends.copy()
         LOG.debug(f"Expecting backends {self.expected_sharding_backends}")
         LOG.debug(f"Initial stats {seen_backends}")
-        for pool in pools:
-            caps = pool['capabilities']
-            backend = caps['volume_backend_name']
-            shard_name = no_shard
-            if 'vcenter-shard' in caps:
-                shard_name = caps['vcenter-shard']
-            volume_type = None
-            if backend in volume_types:
-                volume_type = volume_types[backend]
-            else:
-                LOG.debug(f"Backend {backend} has no associated volume type")
+        type_by_name = cinder_utils.get_volume_types_by_name(self.client)
+        for volume_type_name in pools:
+            for pool in pools[volume_type_name]:
+                caps = pool['capabilities']
+                backend = caps['volume_backend_name']
+                shard_name = no_shard
+                if 'vcenter-shard' in caps:
+                    shard_name = caps['vcenter-shard']
+                volume_type = None
+                if backend in volume_types:
+                    volume_type = volume_types[backend]
+                else:
+                    LOG.debug(f"Backend {backend} has no associated volume type")
 
-            # initialize the accounting for the shard
-            if shard_name not in seen_backends:
-                seen_backends[shard_name] = default_shard_backends.copy()
+                # initialize the accounting for the shard
+                if shard_name not in seen_backends:
+                    seen_backends[shard_name] = default_shard_backends.copy()
 
-            data = self._parse_pool_data(pool, volume_type)
-            pool_name = data['pool']
+                data = cinder_utils.parse_pool_data(pool, volume_type)
+                pool_name = data['pool']
 
-            if shard_name != no_shard:
-                LOG.debug(f"Got stats for {self.region} "
-                          f"{shard_name}/{backend}/{pool_name}")
-            else:
-                LOG.debug(f"Got stats for {self.region} {backend}/{pool_name}")
+                if shard_name != no_shard:
+                    LOG.debug(f"Got stats for {self.region} "
+                            f"{shard_name}/{backend}/{pool_name}")
+                else:
+                    LOG.debug(f"Got stats for {self.region} {backend}/{pool_name}")
 
-            yield from self._report_stats(
-                shard_name, backend, data, caps, quota_obj)
-            if backend in seen_backends[shard_name]:
-                seen_backends[shard_name][backend] += 1
-            elif self.allow_unexpected_backends:
-                LOG.debug(f"Adding unexpected backend {backend} "
-                          f"to shard {shard_name}")
-                seen_backends[shard_name][backend] = 1
-            else:
-                LOG.warning(f"Backend {backend} is not in expected "
-                            f"backends {self.expected_sharding_backends}")
+                yield from self._report_stats(
+                    shard_name, backend, data, caps, quota_obj)
+                if backend in seen_backends[shard_name]:
+                    seen_backends[shard_name][backend] += 1
+                elif self.allow_unexpected_backends:
+                    LOG.debug(f"Adding unexpected backend {backend} "
+                            f"to shard {shard_name}")
+                    seen_backends[shard_name][backend] = 1
+                else:
+                    LOG.warning(f"Backend {backend} is not in expected "
+                                f"backends {self.expected_sharding_backends}")
 
         LOG.debug(f"seen backends {seen_backends}")
         for shard in seen_backends:
@@ -486,3 +420,8 @@ class CinderBackendCollector(BaseCollector.BaseCollector):
                         'Cinder reported free capacity in GiB',
                         0, shard, backend, "None"
                     )
+
+        for pool_name in aggregated_pools:
+            yield from self._report_aggregated_stats(
+                aggregated_pools[pool_name]
+            )
